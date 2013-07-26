c8a79114515236af5e06cf3c4df4e578f06b81f6
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-26 17:28:01 -0500

    move survey create logic to model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,4 +5,11 @@ class Survey < ActiveRecord::Base
    has_many :questions
    has_many :takers, :through => :reponses
    belongs_to :creator, class_name: 'User'
+
+   def self.build_survey(params, current_user)
+    survey = self.create(name: params[:name], creator_id: current_user.id)
+    question = survey.questions.create(text: params[:question])
+    question.options.create(params[:options])
+    return survey
+   end
 end
